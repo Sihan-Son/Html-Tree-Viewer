6125d832c68e2757d4cc33ca8ff7f5e357c1740b
SiHan-Son <sihan830@gmail.com>
2018-06-26 13:02:11 +0900

    init

---
+++ SummerProject/HTMLBrowser/HTMLBrowser/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTMLBrowser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string test = "<p> < /p>";
+            var bracketError = 0;
+
+            test = test.Replace("</ ","</");
+            test = test.Replace("< /", "</");
+
+            Stack<string> bracket = new Stack<string>();
+
+            for (int i =0; i<test.Length;i++)
+            {
+                if(test[i].ToString() == "<" )
+                {
+                    bracket.Push(test[i].ToString());
+                }
+
+                if (test[i].ToString() == ">")
+                {
+                 
+                    try
+                    {
+                        bracket.Pop();
+                    }
+                    catch (Exception e)
+                    {
+                        bracketError++;
+                    }
+
+                }
+
+            }
+
+
+
+            if (bracketError != 0)
+                Console.WriteLine(bracketError + "개의 괄호가 짝이 맞지 않습니다");
+            else
+                Console.WriteLine("모든 괄호의 짝이 맞습니다");
+
+            Console.WriteLine(test);
+
+        }
+    }
+}
