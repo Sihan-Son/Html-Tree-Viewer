5bb8ca5ae565ce0b68ad4078b462de9e4e0797fb
Sihan-Son <sihan830@gmail.com>
2018-06-30 16:10:03 +0900

    up

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label4 = new System.Windows.Forms.Label();
+            this.fileTextBox = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 27);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(323, 374);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "컴파일 할 HTML 코드";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(447, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "컴파일 된 HTML 코드";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(12, 491);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(770, 89);
+            this.richTextBox3.TabIndex = 4;
+            this.richTextBox3.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 476);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Error Return";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(341, 198);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 33);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "ㄱㄱ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "제발 텍스트 파일만 넣으세요";
+            this.openFileDialog1.Filter = "텍스트 파일 (*.txt)|*.txt|HTML 파일 (*.html)|*.html";
+            this.openFileDialog1.InitialDirectory = "C:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 407);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(199, 12);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "혹은, HTML이나 TXT파일 불러오기";
+            // 
+            // fileTextBox
+            // 
+            this.fileTextBox.Location = new System.Drawing.Point(12, 423);
+            this.fileTextBox.Name = "fileTextBox";
+            this.fileTextBox.Size = new System.Drawing.Size(216, 21);
+            this.fileTextBox.TabIndex = 8;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(234, 407);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 37);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "가져오기";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 451);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(9, 12);
+            this.label5.TabIndex = 10;
+            this.label5.Text = " ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 450);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(244, 12);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "! 주의 : 모든 파일은 UTF-8로 인코딩 됩니다";
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(449, 27);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(333, 417);
+            this.webBrowser1.TabIndex = 12;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(341, 251);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(102, 31);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "브라우저로 보기";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(794, 596);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.fileTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox fileTextBox;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var bracketError = 0;
+
+
+            string[] test = new string[] { richTextBox1.Text };
+
+            if (richTextBox1.Lines.Length == 0)
+            {
+                richTextBox3.Text = "컴파일 할 텍스트가 없습니다.";
+                return;
+            }
+
+            for (int x = 0; x >= richTextBox1.Lines.Length; x++)
+            {
+                test[x] = richTextBox1.Lines[x];
+
+            }
+
+            for (int i = 0; i < test.Length; i++)
+            {
+                test[i] = test[i].Replace("</ ", "</");
+                test[i] = test[i].Replace("< /", "</");
+                test[i] = test[i].Replace("< / ", "</");
+            }
+
+            Stack<string> bracket = new Stack<string>();
+            Stack<string> ErrorLine = new Stack<string>();
+
+            for (int i = 0; i < test.Length; i++)
+            {
+                for (int j = 0; j < test[i].Length; j++)
+                {
+                    if (test[i][j].ToString() == "<")
+                    {
+                        bracket.Push(test[i][j].ToString());
+                    }
+
+                    if (test[i][j].ToString() == ">")
+                    {
+
+                        try
+                        {
+                            bracket.Pop(); //stack이 비어 있으면 에러 발생
+                            //richTextBox2.Text = "/123";
+                        }
+                        catch (Exception)
+                        {
+                            bracketError++;
+                            
+                        }
+
+                    }
+                }
+
+            }
+
+
+            if (bracket.Count() != 0) {
+                richTextBox3.Text = "닫히지 않은 괄호가 " + bracket.Count() + "개 존재합니다";
+            }
+                
+
+
+            else if (bracketError != 0)
+            {
+                richTextBox3.Text = bracketError + "개의 괄호가 짝이 맞지 않습니다";
+            } else {
+                richTextBox3.Text = "모든 괄호의 짝이 맞습니다\n\n";
+            }
+                
+
+            Console.WriteLine();
+            for (int i = 0; i < test.Length; i++)
+            {
+                Console.WriteLine(test[i]);
+            }
+                
+            Console.WriteLine();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            fileTextBox.Clear();
+            String file_path = null;
+            openFileDialog1.InitialDirectory = "C:\\";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+
+            {
+                file_path = openFileDialog1.FileName;
+                fileTextBox.Text = file_path;
+                richTextBox1.Text = File.ReadAllText(file_path,Encoding.UTF8);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string url = fileTextBox.Text;
+            webBrowser1.Navigate(url);
+        }
+    }
+}
